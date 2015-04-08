255c04109e855e8b1dd8a7b7076b970a527e1844
Matt Parsons <parsonsmatt@gmail.com>
2015-04-07 21:43:50 -0400

    Add test to initialize Rails app if Gemfile not present

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,7 @@
 
 # Install rvm and ruby:
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-2.2.1 --with-default-gems="bundler"
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-2.2.1 --with-default-gems="bundler rails"
 echo 'gem: --no-document' >> ~/.gemrc
 echo "Sourcing RVM.."
 source ~/.rvm/scripts/rvm
@@ -11,9 +11,15 @@ source ~/.rvm/scripts/rvm
 echo "Going to Vagrant directory..."
 cd /vagrant
 
-echo "Doing bundle install..."
-bundle install
-rake db:setup
+if [ -f Gemfile ]; then
+	echo "Doing bundle install..."
+	bundle install
+	rake db:setup
+else
+	echo "Initializing Rails app..."
+	rails new . --database=postgresql
+fi
+
 gem install foreman
 
 # For convenience:
