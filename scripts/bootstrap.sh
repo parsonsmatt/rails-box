703bbee9d266696a554c2e2a618f71fab2bf0219
Matt Parsons <parsonsmatt@gmail.com>
2015-06-01 13:20:56 -0400

    update ruby version

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,7 @@
 
 # Install rvm and ruby:
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-2.2.1 --with-default-gems="bundler rails"
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-2.2.2 --with-default-gems="bundler rails"
 echo 'gem: --no-document' >> ~/.gemrc
 echo "Sourcing RVM.."
 source ~/.rvm/scripts/rvm
